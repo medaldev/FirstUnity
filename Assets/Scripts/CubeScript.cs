f47cb9aa7936c782f2fb7233de81ad8b2feb179e
Alexey Medvedev <45236468+medaldev@users.noreply.github.com>
2020-11-10 14:27:46 +0300

    Some fixes

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading;
 using UnityEngine;
 
@@ -14,56 +15,56 @@ public class CubeScript : MonoBehaviour {
     public float cubeSpeed;
 
     public float S;
+
+    float Ver, Hor;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void move()
+    {
+        Ver = Input.GetAxis("Vertical") * Time.deltaTime * 10;
+        Hor = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+        Cube.transform.Translate(new Vector3(Hor, 0, Ver));
+    }
+
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKey(KeyCode.UpArrow))
+       if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            Cube.transform.position += (new Vector3(0, 0, cubeSpeed) * Time.deltaTime * Time.deltaTime);
+            move();
         }
 
-       if (Input.GetKey(KeyCode.DownArrow))
+       if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            Cube.transform.position += (new Vector3(0, 0, -cubeSpeed) * Time.deltaTime * Time.deltaTime);
+            move();
         }
 
-       if (Input.GetKey(KeyCode.RightArrow))
+       if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            Cube.transform.position += (new Vector3(cubeSpeed, 0, 0) * Time.deltaTime * Time.deltaTime);
+            move();
         }
 
-       if (Input.GetKey(KeyCode.LeftArrow))
+       if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            Cube.transform.position += (new Vector3(-cubeSpeed  , 0, 0) * Time.deltaTime * Time.deltaTime);
+            move();
         }
        
        if (Input.GetMouseButton(0))
         {
-    // Smoothly tilts a transform towards a target rotation.
-            float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
-            float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+            var mousePosition = Input.mousePosition;
+            mousePosition.z = transform.position.z - Camera.main.transform.position.z; // это только для перспективной камеры необходимо
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); //положение мыши из экранных в мировые координаты
+            var angle = 180 + 90 + Vector2.Angle(Vector2.right, mousePosition - transform.position);//угол между вектором от объекта к мыше и осью х
+            transform.eulerAngles = new Vector3(0f, transform.position.z < mousePosition.z ? angle : -angle, 0f);//немного магии на последок
 
-            Vector3 point = new Vector3();
-            var mousePos = Input.mousePosition;
-            point = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, S));
-            float angle = Mathf.Atan2(mousePos.y - Cube.transform.position.y, mousePos.x - Cube.transform.position.x) * Mathf.Rad2Deg;
-
-            // Rotate the cube by converting the angles into a quaternion.
-            Quaternion target = Quaternion.Euler(0, -Math.Abs(angle) * 10, 0);
-
-
-
-            // Dampen towards the target rotation
-            Cube.transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+           
         }
 
-        
+
 
     }
 
